1eea8598ab731a7387ed4e6499c87285e02dff43
Woo SeungHun <woosh98@gmail.com>
2023-11-26 20:40:59 +0900

    add gethttp

---
+++ app/pages/api/gethttp.ts
@@ -0,0 +1,25 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  switch (req.method) {
+    case 'GET':
+      // GET 요청 처리
+      res.status(200).json({ message: 'This is a GET response' });
+      break;
+
+    case 'POST':
+      // POST 요청 처리
+      res.status(200).json({ message: 'This is a Post response' });
+      // ... 여기에 POST 처리 로직 ...
+      console.log(req.body);
+      
+      break;
+
+    default:
+      // 다른 HTTP 메서드에 대한 처리
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
