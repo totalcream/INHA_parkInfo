33750da258b5c7b1213cab348a9bb570d642aa0a
imgoaaaat <jjs000115@gmail.com>
2023-11-27 23:25:48 +0900

    커밋메세지

--- app/pages/parkinfo/hightech/page.tsx
+++ app/pages/parkinfo/hightech/page.tsx
@@ -10,47 +10,45 @@ import styled from "styled-components";
 //     color: red;
 //     `
 
-export default function Page() {
-    return (
-        <div>
-            <h4 className="title">hightech</h4>
-            <div className="cart-item">
-                <p>주차장 이름</p>
-                <p>여석</p>
-                <p>전체 좌석</p>
-            </div>
+export default function Cart() {
+  // 가정: 주어진 데이터 배열 (0 또는 1 값)
+  const data = [1, 1, 0, 1, 0, 1, 0, 1, 0];
 
-            <CartItem/>
-
-            <div className="high">
-            <Image
-            src="/high1.png"
-            alt={"1"}
-            className="park1_img"
-            layout="fixed"
-            width={1362}
-            height={174}
-            ></Image>
-            {Array.from({ length: 9 }, (_, index) => (
-                <Image
-                    key={index} src="/green1.png"
-                    width={1728}
-                    height={1080}
-                    layout="fixed"
-                    alt={`주차장 이미지 ${index + 1}`}
-                    className="green1-img"></Image>
-            ))}
-            </div>
-        </div>
-    );   
-  }
-
-  function CartItem() {
-    return (
+  return (
+    <div>
+      <h4 className="title">하이테크 주차장 현황</h4>
       <div className="cart-item">
-        <p>하이테크 주차장</p>
-        <p>n칸</p>
-        <p>9칸</p>
+        <p>주차장이름</p>
+        <p>여석</p>
+        <p>전체좌석</p>
       </div>
-    );
-  }
+
+      <CartItem />
+
+      <div className="high">
+  <img src="/high1.png" className="park1-img" />
+  <div className="image-container">
+    {data.map((value, index) => (
+      <img
+        key={index}
+        src={value === 0 ? '/green1.png' : '/red1.png'}
+        alt={`주차장 이미지 ${index + 1}`}
+        className="green1-img"
+      />
+    ))}
+  </div>
+</div>
+
+    </div>
+  );
+}
+
+function CartItem() {
+  return (
+    <div className="cart-item">
+      <p>하이테크 주차장</p>
+      <p>n칸</p>
+      <p>9칸</p>
+    </div>
+  );
+}
