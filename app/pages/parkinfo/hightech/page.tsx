a5e86331f72db1bbd954843c8d84e07d790148f6
Woo SeungHun <woosh98@gmail.com>
2023-11-28 21:58:02 +0900

    edit hightech page

--- app/pages/parkinfo/hightech/page.tsx
+++ app/pages/parkinfo/hightech/page.tsx
@@ -4,7 +4,6 @@
 // import { unstable_noStore as noStore } from "next/cache";
 // import { useState } from "react";
 import Image from "next/image";
-<<<<<<< HEAD
 // import styled from "styled-components";
 
 // 2023-11-28 수정
@@ -12,11 +11,9 @@ import Image from "next/image";
 // const P = styled.p`
 //     color: red;
 //     `
-=======
+
 import styled from "styled-components";
 import Link from 'next/link';
->>>>>>> a441bd890b6ff438aea1c0c540cb5a327a7cf897
-
 export default function Cart() {
   // 가정: 주어진 데이터 배열 (0 또는 1 값)
   const data = [1, 1, 0, 1, 0, 1, 0, 1, 0];
@@ -56,7 +53,7 @@ export default function Cart() {
   );
 }
 
-function CartItem({ k }) {
+function CartItem({ k}) {
   return (
     <div className="cart-item">
       <p>하이테크 </p>
